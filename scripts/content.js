2c158fa0aef6039d815eb5d71de6929488292077
hitakritgoplani <hitakritgoplani@gmail.com>
2025-04-20 09:32:14 +0530

    new record hot key

--- scripts/content.js
+++ scripts/content.js
@@ -1,23 +1,22 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    let currentUrl = window.location.href;
+    let match = currentUrl.match(/o\/([^\/]+)\//);
+    let objName = match && match[1] ? match[1] : '';
+        
     if (request.action === "clickNewButton") {
         console.log('in new record case');
-        var listItem = document.querySelector("li[data-target-selection-name='sfdc:StandardButton.Account.New']");
+        let listItem = document.querySelector(`li[data-target-selection-name='sfdc:StandardButton.${objName}.New']`);
 
         if (listItem) {
             console.log('List item found');
-
-            // Find the anchor tag within the list item
-            var anchorTag = listItem.querySelector('a');
-
+            let anchorTag = listItem.querySelector('a');
             if (anchorTag) {
-                console.log('Anchor tag found');
-
-                // Method 1: Standard click
                 anchorTag.click();
-                console.log('Standard click attempted on anchor tag');
             } else {
-                console.error("Could not find the 'New' button.");
+                console.error(`Could not find anchor tag within the '${objName}' New button.`);
             }
+        } else {
+            console.error(`'Could not find ${objName}.New'`);
         }
     }
 });
