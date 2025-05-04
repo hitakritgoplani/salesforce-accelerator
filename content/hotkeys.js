d9fb5132f2fc232c27ba3fd2a2f6b09d91218b84
hitakritgoplani <hitakritgoplani@gmail.com>
2025-05-04 14:17:48 +0530

    fixed the structure

---
+++ content/hotkeys.js
@@ -0,0 +1,20 @@
+let shortcutMap = {};
+
+chrome.storage.sync.get('userShortcuts', function(data) {
+    shortcutMap = data.userShortcuts || {};
+    console.log('Loaded user shortcuts from storage:', shortcutMap);
+});
+
+function handleHotkey(keyData) {
+    // Check if Shift is pressed AND the pressed key is a single character
+    if (keyData.shiftKey && keyData.key.length === 1) {
+        const currentUrl = window.location.href;
+        const orgDomain = new URL(currentUrl).origin;
+        const shortcutInfo = shortcutMap[keyData.key.toLowerCase()]; // Get the shortcut object
+
+        if (shortcutInfo && shortcutInfo.link) { // Check if the object and the link exist
+            const finalURL = shortcutInfo.link.startsWith('/') ? `${orgDomain}${shortcutInfo.link}` : shortcutInfo.link;
+            window.open(finalURL, '_blank');
+        }
+    }
+}
